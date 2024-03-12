f93eeb6a461aabc78a0e96a778de18dfc54a3548
Irina <i.sus2011@yandex.ru>
2024-03-12 14:54:37 +0300

    Закрытие всплывающего окна при клике на оверлей

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { useNavigate } from 'react-router-dom';
+import "./NavTab.css";
+import Dropdown from "../Dropdown/Dropdown";
 
-import './NavTab.css';
-import Dropdown from '../Dropdown/Dropdown';
-
-const NavTab = ({ themeClass}) => {
-
+const NavTab = ({ themeClass }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
 
@@ -14,32 +12,31 @@ const NavTab = ({ themeClass}) => {
 
   const navItems = [
     {
-      label: 'О ГЛАВНОМ',
-      path: '/about-project',
+      label: "О ГЛАВНОМ",
+      path: "/about-project",
       content: [
-
-        { item: 'Для кого', id: "for-whom" },
-        { item: 'Что изменится', id: "will-change" },
-        { item: 'Как проходят коуч-сессии', id: "process" },
-        { item: 'Часто задаваемые вопросы', id: "questions" },
-      ]
+        { item: "Для кого", id: "for-whom" },
+        { item: "Что изменится", id: "will-change" },
+        { item: "Как проходят коуч-сессии", id: "process" },
+        { item: "Часто задаваемые вопросы", id: "questions" },
+      ],
     },
     {
-      label: 'ОБО МНЕ',
-      path: '/about-me',
+      label: "ОБО МНЕ",
+      path: "/about-me",
       content: [
-        { item: 'Почему мне доверяют', id: 'trust-me' },
-        { item: 'Обо мне', id: 'my-education' },
-        { item: 'Дипломы и сертификаты', id: 'my-diplomas' },
-        { item: 'Отзывы', id: 'reviews' },
-      ]
+        { item: "Почему мне доверяют", id: "trust-me" },
+        { item: "Обо мне", id: "my-education" },
+        { item: "Дипломы и сертификаты", id: "my-diplomas" },
+        { item: "Отзывы", id: "reviews" },
+      ],
     },
     {
-      label: 'СТОИМОСТЬ',
-      path: '/price',
+      label: "СТОИМОСТЬ",
+      path: "/price",
       id: "price",
     },
-    { label: 'КОНТАКТЫ', path: '/contacts', id: "contacts", },
+    { label: "КОНТАКТЫ", path: "/contacts", id: "contacts" },
   ];
 
   const handleItemHover = (index) => {
@@ -56,7 +53,7 @@ const NavTab = ({ themeClass}) => {
     setIsModalOpen(false);
 
     // определяем высоту хедера
-    const header = document.querySelector('.header');
+    const header = document.querySelector(".header");
     const headerHeight = header ? header.offsetHeight : 0;
 
     navigate(path);
@@ -69,56 +66,68 @@ const NavTab = ({ themeClass}) => {
 
           window.scrollTo({
             top: elementOffset,
-            behavior: 'smooth',
+            behavior: "smooth",
           });
         }
       }
     }, 0);
   };
 
+  useEffect(() => {
+    const handleOutsideClick = (e) => {
+      if (isModalOpen && e.target.closest(".nav-tab") === null) {
+        setIsModalOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isModalOpen]);
+
   return (
     <nav className={`nav-tab ${themeClass}`}>
-
       <ul className={`nav-tab__list ${themeClass}`}>
         {navItems.map((item, index) => (
           <li
             key={index}
-            className='nav-tab__item '
-
+            className="nav-tab__item "
             onMouseEnter={() => handleItemHover(index)}
             onClick={() => handleItemToggle(index)}
-
           >
-            <div className='nav-tab__item-container'>
+            <div className="nav-tab__item-container">
               {item.content ? (
-
                 // у элемента есть content, используем Dropdown
-                // <button className='nav-tab__link'
-                <button className={`nav-tab__link ${themeClass}`}
+                <button
+                  className={`nav-tab__link ${themeClass}`}
                   onClick={() => handleItemClick(item.path, item.id)}
-                >{item.label}</button>
+                >
+                  {item.label}
+                </button>
               ) : (
                 // у элемента нет content, переходите напрямую
-                // <button
-                  // className='nav-tab__link'
-                  <button className={`nav-tab__link ${themeClass}`}
+                <button
+                  className={`nav-tab__link ${themeClass}`}
                   onClick={() => navigate(item.path)}
                 >
                   {item.label}
                 </button>
               )}
             </div>
-            {isModalOpen && selectedItem && selectedItem.label === item.label && (
-              <Dropdown
-                content={selectedItem.content}
-
-                onItemClick={(sectionId) => handleItemClick(item.path, sectionId)}
-
-                onMouseEnter={() => setIsModalOpen(true)}
-                onMouseLeave={() => setIsModalOpen(false)}
-
-              />
-            )}
+            {isModalOpen &&
+              selectedItem &&
+              selectedItem.label === item.label && (
+                <Dropdown
+                  content={selectedItem.content}
+                  onItemClick={(sectionId) =>
+                    handleItemClick(item.path, sectionId)
+                  }
+                  onMouseEnter={() => setIsModalOpen(true)}
+                  onMouseLeave={() => setIsModalOpen(false)}
+                />
+              )}
           </li>
         ))}
       </ul>
@@ -127,4 +136,3 @@ const NavTab = ({ themeClass}) => {
 };
 
 export default NavTab;
-
